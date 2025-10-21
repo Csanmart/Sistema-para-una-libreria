f0c0c29a9ade87eaaedf5bb7e3eddce9f486cd59
Csanmart <csanmart2024@hotmail.com>
2025-10-21 13:05:57 -0500

    Subo el controlador de libro y las routers

--- Backend/routers/libros_routes.js
+++ Backend/routers/libros_routes.js
@@ -5,6 +5,7 @@ const librosController = require('../controllers/libros_controllers');
 router.post('/libros/crearLibros', librosController.CrearLibro);
 router.get('/libros/mostrarLibros', librosController.MostrarTodosLibros);
 router.get('/libros/mostrarLibro/:id_libro', librosController.MostrarPorId);
-
+router.put('/libros/actualizarLibro/:id_libro', librosController.ActualizarLibro);
+router.delete('/libros/eliminarLibros/:id_libro', librosController.EliminarLibro)
 
 module.exports = router;
